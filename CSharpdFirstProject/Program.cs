d526f48b3744e4e0afe2d6d677dbe612826beff3
Alaa <alaar@A_LAPTOP>
2026-06-30 20:18:49 +0400

    added input sections for 2 numbers, then preform mathmatical operations.

--- CSharpdFirstProject/Program.cs
+++ CSharpdFirstProject/Program.cs
@@ -9,16 +9,31 @@
 
             Console.Write("Please enter your name: ");
             String Username = Console.ReadLine();
-            Console.WriteLine("Hello"  + Username); //printing hello with the username
+            Console.WriteLine("Hello"  + Username + "welcome to Spark"); //printing hello with the username
 
 
             Console.WriteLine("please enter your age: ");
             int age = int.Parse(Console.ReadLine()); //using int.parse to convert the string input to an integer
-            Console.WriteLine("You are " + age + " years old.");
 
             Console.WriteLine("enter your salary: ");
             float salary = float.Parse(Console.ReadLine()); //using float.parse to convert the string input to a float
 
+
+
+
+            Console.WriteLine("enter first number: ");
+            float num1 = float.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("enter second number: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float sum = num1 + num2; //adding the two numbers
+            float difference = num1 - num2; //subtracting the two numbers
+            float multiplication = num1 * num2; //multiplying the two numbers
+            float division = num1 / num2; //dividing the two numbers
+            float modulus = num1 % num2; //modulus of the two numbers
+
+            
         }
 
 
