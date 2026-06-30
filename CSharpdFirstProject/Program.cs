e0bac95d9e5275bb7946f7983a6919776ac57fa6
Alaa <alaar@A_LAPTOP>
2026-06-30 20:03:49 +0400

    Added input section for user's age

--- CSharpdFirstProject/Program.cs
+++ CSharpdFirstProject/Program.cs
@@ -7,9 +7,19 @@
         {
             Console.WriteLine("Hello, SparktoCode!");
 
-            Console.WriteLine("Please enter your name:");
+            Console.Write("Please enter your name: ");
             String Username = Console.ReadLine();
-            Console.WriteLine("Hello" + Username); //printing hello with the username
+            Console.WriteLine("Hello"  + Username); //printing hello with the username
+
+
+            Console.WriteLine("please enter your age: ");
+            int age = int.Parse(Console.ReadLine()); //using int.parse to convert the string input to an integer
+            Console.WriteLine("You are " + age + " years old.");
+
+          
         }
+
+
+
     }
 }
