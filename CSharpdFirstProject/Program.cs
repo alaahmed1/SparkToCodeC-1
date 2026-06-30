714aea4f801ded576f662c334aef49bd8b94faeb
Alaa <alaar@A_LAPTOP>
2026-06-30 19:55:32 +0400

    added username input section and printing hello with their username

--- CSharpdFirstProject/Program.cs
+++ CSharpdFirstProject/Program.cs
@@ -6,6 +6,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, SparktoCode!");
+
+            Console.WriteLine("Please enter your name:");
+            String Username = Console.ReadLine();
+            Console.WriteLine("Hello" + Username); //printing hello with the username
         }
     }
 }
