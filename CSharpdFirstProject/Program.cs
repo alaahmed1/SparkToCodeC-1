74b12c67faf177624320f03c40c280b048053da1
Alaa <alaar@A_LAPTOP>
2026-06-30 19:41:07 +0400

    Added my C# project folder inside the SparkToCode folder

---
+++ CSharpdFirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpdFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
