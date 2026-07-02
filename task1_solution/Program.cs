f69b99026bdf2f656c07a63558c9c76e2f65fd67
Alaa <alaar@A_LAPTOP>
2026-07-02 14:58:53 +0400

    added task1_solution to solve 10 problems and parctice C# fundementals

---
+++ task1_solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task1_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
