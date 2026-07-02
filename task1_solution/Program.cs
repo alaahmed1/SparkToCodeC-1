eb50ca9c8dee6b229abf228050bad8acbf54e0fb
Alaa <alaar@A_LAPTOP>
2026-07-02 18:41:35 +0400

    solved and tested task 3, commented both tas 2 and 3

--- task1_solution/Program.cs
+++ task1_solution/Program.cs
@@ -16,15 +16,26 @@
             //Console.WriteLine("Liftoff!");
 
 
-            //task 2:
-            Console.WriteLine("enter a positive whole number:");
-            int N = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            for (int count = 0; count <= N; count++)
-            {
-                sum += count;
-            }
-            Console.WriteLine("the sum of numbers from 0 to N is: " + sum);
+            //task 2: Sum of Numbers 1 to N
+            //Console.WriteLine("enter a positive whole number:");
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int count = 0; count <= N; count++)
+            //{
+            //    sum += count;
+            //}
+            //Console.WriteLine("the sum of numbers from 0 to N is: " + sum);
+
+
+            //Task3: Multiplication Table
+            //Console.WriteLine("enter a number to print its multiplication table:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //for (int count = 1; count <= 10; count++)
+            //{
+            //    int multi = number * count;
+            //    Console.WriteLine(number + " x " + count + " = " + multi);
+            //}
+
 
         }
     }
