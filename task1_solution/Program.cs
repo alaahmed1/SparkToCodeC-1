0d2dc43eaf5915f02ceec9b4f1ec64fdbbb0d7d1
Alaa <alaar@A_LAPTOP>
2026-07-02 18:37:14 +0400

    commented on task 1 and solved and tested task 2

--- task1_solution/Program.cs
+++ task1_solution/Program.cs
@@ -4,15 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a starting number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            ////task 1: Countdown Timer
+            //Console.Write("Enter a starting number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
 
-            for (int count = number; count >= 1; count--)
+            //for (int count = number; count >= 1; count--)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+            //Console.WriteLine("Liftoff!");
+
+
+            //task 2:
+            Console.WriteLine("enter a positive whole number:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int count = 0; count <= N; count++)
             {
-                Console.WriteLine(count);
+                sum += count;
             }
+            Console.WriteLine("the sum of numbers from 0 to N is: " + sum);
 
-            Console.WriteLine("Liftoff!");
         }
     }
 }
