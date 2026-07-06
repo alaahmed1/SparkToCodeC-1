0b4fb3dcc280ef613e3cc32a2d8890ee5c74a099
Alaa <alaar@A_LAPTOP>
2026-07-06 07:01:18 +0400

    added a nex branch called Built_in_Functions and then added a new project to start solving part 3 issue

---
+++ Built_in_tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Built_in_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
