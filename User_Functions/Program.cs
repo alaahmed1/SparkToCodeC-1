4efb1b1d94d9e37e7589423ce6c99edc0a76556a
Alaa <alaar@A_LAPTOP>
2026-07-07 00:26:21 +0400

    added a new project named "User_Functions"tosolve tasks of part 4.

---
+++ User_Functions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace User_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
