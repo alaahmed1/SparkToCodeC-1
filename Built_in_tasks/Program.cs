be5873f2f9065ffa0c16c8b1dbdb911322c78da4
Alaa <alaar@A_LAPTOP>
2026-07-06 07:05:00 +0400

    solved and tested task 1

--- Built_in_tasks/Program.cs
+++ Built_in_tasks/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ////task 1: Absolute Value of Difference
+            //Console.Write("enter first number: ");
+            //float firstNumber = Convert.ToSingle(Console.ReadLine());
+            //Console.Write("enter second number: ");
+            //float secondNumber = Convert.ToSingle(Console.ReadLine());
+            //float sub = firstNumber - secondNumber;
+            //Console.WriteLine("the absolute value of the difference between the two numbers is: " + Math.Abs(sub));
+
+
+            //
         }
     }
 }
