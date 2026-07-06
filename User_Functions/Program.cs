db1ad04ab7d4beec975db1ac52460b55834c1543
Alaa <alaar@A_LAPTOP>
2026-07-07 00:53:59 +0400

    solved and tested the square function (task 2)

--- User_Functions/Program.cs
+++ User_Functions/Program.cs
@@ -10,11 +10,20 @@
             //PrintWelcome(userName);
 
 
+            //Task 2: Sqaure number Function
+            //Console.Write("enter a number to calculate its square: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("the square of the number is: " + Square(number));
         }
         //static void PrintWelcome(string name) // Personalized Welcome Function
         //{
         //    Console.WriteLine("Welcome " + name);
         //}
 
+        //static int Square(int number) //Sqaure number Function
+        //{
+        //    int result = number * number;
+        //    return result;
+        //}
     }
 }
