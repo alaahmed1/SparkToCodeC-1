5dd9f19c5b7fb816d4edfd887d2187e8bc78fd70
Alaa <alaar@A_LAPTOP>
2026-07-06 07:08:14 +0400

    solved and tested task 2

--- Built_in_tasks/Program.cs
+++ Built_in_tasks/Program.cs
@@ -13,7 +13,12 @@
             //Console.WriteLine("the absolute value of the difference between the two numbers is: " + Math.Abs(sub));
 
 
-            //
+            //task 2: Power & Root Explorer
+            //Console.WriteLine("enter a number to calculate its square and square root:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //double square = Math.Pow(number, 2);
+            //double squareRoot = Math.Sqrt(number);
+            //Console.WriteLine("the square of the number is: " + square + " ,and the square root is: " + squareRoot);
         }
     }
 }
