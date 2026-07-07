f049f623545223872060a4080923c3880fa8ea2c
Alaa <alaar@A_LAPTOP>
2026-07-07 23:34:02 +0400

    solved odd/even function

--- User_Functions/Program.cs
+++ User_Functions/Program.cs
@@ -16,7 +16,22 @@
             //Console.WriteLine("the square of the number is: " + Square(number));
 
             //Task 4: Display Menu
-            DisplayMenu();
+            //DisplayMenu();
+
+            //task 5: Determine odd/even
+            //Console.WriteLine("enter a number to check if it is odd or even:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool result = IsEven(number);
+
+            //if (result)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
 
         }
         //static void PrintWelcome(string name) // Personalized Welcome Function
@@ -30,12 +45,17 @@
         //    return result;
         //}
 
-        static void DisplayMenu() //task 4
-        {
-            Console.WriteLine("menu");
-            Console.WriteLine("1) Start");
-            Console.WriteLine("2) Help");
-            Console.WriteLine("3) Exit");
-        }
+        //static void DisplayMenu() //task 4
+        //{
+        //    Console.WriteLine("menu");
+        //    Console.WriteLine("1) Start");
+        //    Console.WriteLine("2) Help");
+        //    Console.WriteLine("3) Exit");
+        //}
+
+        //static bool IsEven(int number) //task 5
+        //{
+        //    return number % 2 == 0;
+        //}
     }
 }
