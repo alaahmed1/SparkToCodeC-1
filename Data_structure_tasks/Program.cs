e32094a6d3cc5135c2537b4a5da1a6b0c4c93381
Alaa <alaar@A_LAPTOP>
2026-07-08 00:00:51 +0400

    solved Task 1: Fixed Grades Array

--- Data_structure_tasks/Program.cs
+++ Data_structure_tasks/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //an array is a data structure that can hold a fixed number of values of a single type. The values are stored in contiguous memory locations and can be accessed using an index. Arrays are useful when you need to store a collection of related data, such as a list of numbers or names.
+            //Task 1: Fixed Grades Array
+            //int[] grades = new int[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("Enter grade " + (i + 1) + ": ");
+            //    grades[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+
+            // lists are a data structure that can hold a variable number of values of a single type.
+            // The values are stored in non-contiguous memory locations and can be accessed using an index.
+            // Lists are useful when you need to store a collection of related data that may change in size.
+            //Task 2: Dynamic To-Do List
+
         }
     }
 }
