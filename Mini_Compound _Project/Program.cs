7fc2eb86b8d23a14490e2df214d80495f3fc3bb1
Alaa <alaar@A_LAPTOP>
2026-07-11 19:56:23 +0400

    created a branch and a new project withing to solve the Mini Compound Project

---
+++ Mini_Compound _Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_Compound__Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
