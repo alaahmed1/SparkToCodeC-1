75d4e11295175b1c0d53222410ac99239da3865f
Alaa <alaar@A_LAPTOP>
2026-07-18 18:29:25 +0400

    added a HotelManagementSystem project file to solve the tasks

---
+++ HotelManagementSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelManagementSystem
+
+
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
